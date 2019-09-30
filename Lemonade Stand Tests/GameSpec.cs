f6133837de17a32e4ecae3420c13c336f7f6bff8
gabrielkunkel <henrysaltandson@gmail.com>
2019-09-30 10:13:59 -0500

    initial commit

---
+++ Lemonade Stand Tests/GameSpec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LemonadeStand;
+
+namespace Lemonade_Stand_Tests
+{
+  [TestClass]
+  public class GameSpec
+  {
+    [TestMethod]
+    public void InitialCheck_TestsWorking_GameClass()
+    {
+      Game testGame = new Game();
+      Assert.AreEqual(5, testGame.TestVariable);
+
+    }
+  }
+}
---
+++ Lemonade Stand Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lemonade Stand Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lemonade Stand Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51bf8884-94b0-47be-9c15-b88953bfbff0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
